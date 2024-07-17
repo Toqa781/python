83e7055c41a0c8a7d934f9913b534d3960d9e392
ToqaAbdalla <132062106+Toqa781@users.noreply.github.com>
2024-07-17 20:23:41 +0300

    Update text_analyzer.py

--- text_analyzer.py
+++ text_analyzer.py
@@ -2,25 +2,27 @@ givenString="Lorem ipsum dolor! diam amet, consetetur Lorem magna. sed diam nonu
 
 class TextAnalyzer(object):
     def __init__(self,text):
-        formattedText=text.replace('.','').replace(',','').replace('!','').replace('?','')
-        formattedText=formattedText.lower()
-        self.fmtText=formattedText
+      formattedText=text.replace('.','').replace(',','').replace('?','').replace('!','')
+      formattedText=formattedText.lower()
+      self.fmtText=formattedText
     def freqAll(self):
-        wordList=self.fmtText.split(' ')
-        freqMap={}
-        for word in set(wordList):
-            freqMap[word]=wordList.count(word)
-        
-        return freqMap
-    def freqOf(self,word):
-        freqDict=self.freqAll()
+      wordList=self.fmtText.split(' ')
+      freqMap={}
+      for word in set(wordList):
+         freqMap[word]=wordList.count(word)
 
-        if word in freqDict:
-            return freqDict[word]
-        else:
-            return 0
+      return freqMap
+    def freqOf(self,word):
+       freqDict=self.freqAll()
+       if word in freqDict:
+          return freqDict[word]
+       else:
+          return 0
 
 analyzed=TextAnalyzer(givenString)
+print(analyzed.fmtText)
+
+print(analyzed.freqAll())
 word="lorem"
 freq=analyzed.freqOf(word)
-print(freq)
+print("The word",word,"appears",freq,"times")
